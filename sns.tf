4a89531fd7899ef258b9dd0bd557fe62a805b3e4
Kyle Laker <kyle@laker.email>
2020-06-16 19:47:52 -0400

    Add publish permissions

--- sns.tf
+++ sns.tf
@@ -5,7 +5,8 @@ resource "aws_sns_topic" "email_topic" {
 resource "aws_sns_topic_policy" "read_policy" {
   arn = aws_sns_topic.email_topic.arn
   policy = templatefile("public-read.json", {
-    topic = aws_sns_topic.email_topic.arn
+    topic    = aws_sns_topic.email_topic.arn,
+    function = aws_lambda_function.check-function
     }
   )
 }
