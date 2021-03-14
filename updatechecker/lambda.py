8d5f23e02c7446965064cbcb486f52bc327c9cdd
Kyle Laker <kyle@laker.email>
2021-03-13 19:09:31 -0500

    Refactor logic into separate functions

--- updatechecker/lambda.py
+++ updatechecker/lambda.py
@@ -1,4 +1,5 @@
 import json
+from json.decoder import JSONDecodeError
 import os
 
 import boto3
@@ -12,28 +13,28 @@ from checkers.finch_py import FinchChecker
 from checker import BaseUpdateCheckerEncoder
 
 
-_S3_PREFIX = os.environ.get('S3_PREFIX', 'updatechecker').rstrip('/')
+_S3_PREFIX = os.environ.get("S3_PREFIX", "updatechecker").rstrip("/")
 
-s3 = boto3.client('s3')
-sns = boto3.client('sns')
+s3 = boto3.client("s3")
+sns = boto3.client("sns")
 
 
 def s3_key(software):
     return f"{_S3_PREFIX}/{software}.json"
 
 
-def load_data(s3, bucket, key):
+def load_stored_data(s3, bucket, key):
     body = s3.get_object(
         Bucket=bucket,
         Key=key,
-    )['Body']
+    )["Body"]
     return json.load(body)
 
 
 def remap_data(data):
     remapped = {}
     for entry in data:
-        remapped[entry['software']] = entry['latest']
+        remapped[entry["software"]] = entry["latest"]
     return remapped
 
 
@@ -48,11 +49,7 @@ def notify(name, data, topic):
         SHA1: {data['sha1']}
         Beta: {data['beta']}
     """
-    sns.publish(
-        TopicArn=topic,
-        Subject=subject,
-        Message=message
-    )
+    sns.publish(TopicArn=topic, Subject=subject, Message=message)
 
 
 def notify_failure(name, err, topic):
@@ -62,39 +59,47 @@ def notify_failure(name, err, topic):
     Error details:
     {err}
     """
-    sns.publish(
-        TopicArn=topic,
-        Subject=subject,
-        Message=message
-    )
+    sns.publish(TopicArn=topic, Subject=subject, Message=message)
+
+
+def configure_session():
+    session = requests.Session()
+    session.headers.update({"User-Agent": "Update checking tool"})
+    return session
+
+
+def get_latest_version_data(checker, context, session, beta=None):
+    if beta is None:
+        beta = os.environ.get("beta", False)
+    checker_instance = checker(context, session, beta)
+    version_data_json = json.dumps(checker_instance, cls=BaseUpdateCheckerEncoder)
+    new_data = json.loads(version_data_json)["latest"]
+    return new_data
 
 
 def handler(event, context):
-    bucket = os.environ['bucket']
-    email_topic = os.environ['email_topic']
-    beta = os.environ.get('beta', False)
-    force_notify = event.get('force-notify', False)
+    bucket = os.environ["bucket"]
+    email_topic = os.environ["email_topic"]
+    beta = os.environ.get("beta", False)
+    force_notify = event.get("force-notify", False)
 
-    session = requests.Session()
-    session.headers.update({'User-Agent': 'Update Check tool'})
+    session = configure_session()
     context = {
-        'eclipse': {
-            'mirror_url': 'http://mirror.math.princeton.edu/pub/eclipse/technology/epp/downloads/release',
+        "eclipse": {
+            "mirror_url": "http://mirror.math.princeton.edu/pub/eclipse/technology/epp/downloads/release",
         },
     }
     notifications = []
     for chk in [EclipseJavaChecker, JGraspChecker, FinchChecker]:
         try:
-            checker_instance = chk(context, session, beta)
-            new_json = json.dumps(checker_instance, indent=4, cls=BaseUpdateCheckerEncoder)
+            new_data = get_latest_version_data(chk, context, session, beta)
         except Exception as exc:
             notify_failure(chk.name, exc, email_topic)
             continue
 
-        new_data = json.loads(new_json)['latest']
         try:
-            old_data = load_data(s3, bucket, s3_key(chk.short_name))
-        except:
+            old_data = load_stored_data(s3, bucket, s3_key(chk.short_name))
+        except (ClientError, JSONDecodeError):
             old_data = []
 
         # There's not even a need to re-write the data if there is not a new version
@@ -102,14 +107,13 @@ def handler(event, context):
         if new_data == old_data and not force_notify:
             continue
 
-        print(f"Old: {json.dumps(old_data)}")
-        print(f"New: {json.dumps(new_data)}")
-
         notify(chk.name, new_data, email_topic)
         notifications.append(chk.name)
         try:
-            s3.put_object(Bucket=bucket, Key=s3_key(chk.short_name), Body=json.dumps(new_data))
+            s3.put_object(
+                Bucket=bucket, Key=s3_key(chk.short_name), Body=json.dumps(new_data)
+            )
         except ClientError as err:
             notify_failure(chk.name, f"Unable to write to S3: {err}", email_topic)
 
-    return {'notifications': notifications}
+    return {"notifications": notifications}
